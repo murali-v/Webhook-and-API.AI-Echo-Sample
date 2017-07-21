73edd5706f2cd43d302cb1efe001866a7b0ac78f
murali-v <murali@looksee.do>
2017-07-21 14:08:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -80,3 +80,4 @@ restService.post('/slack-test', function(req, res) {
 restService.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
 });
+*/
