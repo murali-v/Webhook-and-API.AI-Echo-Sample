7dcb72869f0f20f067497b811bff0da84164d314
murali-v <murali@looksee.do>
2017-07-24 13:47:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ expressApp.post('/echo', function (req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
         speech: data,
-        displayText: speech,
+        displayText: data,
         source: 'webhook-echo-sample',
         data: data
     });
