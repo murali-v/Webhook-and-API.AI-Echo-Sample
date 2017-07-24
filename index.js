e35939d4542ec73f378be3002109314e7c13d9c4
murali-v <murali@looksee.do>
2017-07-24 11:34:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ var apiai = require('apiai');
 
 var app = apiai("e6483d023e1a4ef2a63038636ea3ec3e");
 
-var request = app.textRequest('Hello', {
-    sessionId: 'aa25ee14-0efb-4292-9bdf-dccf69063db1'
-});
+var express = require('express')
+var expressApp = express()
  
-request.on('response', function(response) {
-    console.log(response);
-});
- 
-request.on('error', function(error) {
-    console.log(error);
-});
- 
-request.end();
+expressApp.get('/testHello', function (req, res) {
+    var request = app.textRequest('Hello', {
+        sessionId: 'aa25ee14-0efb-4292-9bdf-dccf69063db1'
+    });
+    
+    request.on('response', function(response) {
+        console.log(response);
+    });
+
+    request.on('error', function(error) {
+        console.log(error);
+    });
+
+    request.end();
+})
+
+expressApp.listen(3000)
