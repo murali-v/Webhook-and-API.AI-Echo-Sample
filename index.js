172bfca83b3a19c501e4b859bc3c3b9015a357d5
murali-v <murali@looksee.do>
2017-07-24 11:38:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ var app = apiai("e6483d023e1a4ef2a63038636ea3ec3e");
 
 var express = require('express')
 var expressApp = express()
- 
+
+expressApp.get('/testHelloSimple', function (req, res) {
+    res.send("Hello");
+});
+
 expressApp.get('/testHello', function (req, res) {
     var request = app.textRequest('Hello', {
         sessionId: 'aa25ee14-0efb-4292-9bdf-dccf69063db1'
