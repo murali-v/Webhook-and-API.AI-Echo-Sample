a0d6c5cc3e228a798fa65eb8c69ee595945e54a7
murali-v <murali@looksee.do>
2017-07-24 12:53:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ expressApp.use(bodyParser.urlencoded({
 expressApp.use(bodyParser.json());
 expressApp.post('/echo', function (req, res) {
     
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    var speech = req.body.result //&& req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
         speech: speech,
         displayText: speech,
