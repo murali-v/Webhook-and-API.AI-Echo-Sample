b6f4dac19aa70c30fbf3d48b7fa2389716a034f0
murali-v <murali@looksee.do>
2017-07-24 10:00:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ var request = app.textRequest('Hello', options);
 
 request.on('response', function(response) {
     console.log(response);
+    Body:
+    {
+    "speech": "Barack Hussein Obama II was the 44th and current President of the United States.",
+    "displayText": "Barack Hussein Obama II was the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+    "data": {...},
+    "contextOut": [...],
+    "source": "DuckDuckGo"
+    }
+    console.log(Body);
 });
 
 request.on('error', function(error) {
