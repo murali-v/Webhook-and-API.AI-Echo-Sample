2d70177579870407c717697a59aa16ecc2a02224
murali-v <murali@looksee.do>
2017-07-28 13:28:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
-var apiai = require('apiai');
+exports.handler = (event, context, callback) => {
 
-var app = apiai("e6483d023e1a4ef2a63038636ea3ec3e");
-const bodyParser = require('body-parser');
+    // VIEW DOCS HERE:  https://github.com/MotionAI/nodejs-samples
 
-var express = require('express')
-var expressApp = express()
+    /* "event" object contains payload from Motion AI
+        {
+            "from":"string", // the end-user's identifier (may be FB ID, email address, Slack username etc, depends on bot type)
+            "session":"string", // a unique session identifier
+            "botId":"string", // the Motion AI ID of the bot
+            "botType":"string", // the type of bot this is (FB, Slack etc)
+            "customPayload":"string", // a developer-defined payload for carrying information
+            "moduleId":"string", // the current Motion AI Module ID
+            "moduleNickname":"string", // the current Motion AI Module's nickname
+            "inResponseTo":"string", // the Motion AI module that directed the conversation flow to this module
+            "reply":"string", // the end-user's reply that led to this module
+            "result":"string" // any extracted data from the prior module, if applicable,
+            "replyHistory":"object" // an object containing the current session's conversation messages
+            "nlpData":"object" // stringified NLP data object parsed from a user's message to your bot if NLP engine is enabled
+            "customVars":"string" // stringified object containing any existing customVars for current session
+            "fbUserData":"string" // for Messenger bots only - stringified object containing user's meta data - first name, last name, and id
+            "attachedMedia":"string" // for Messenger bots only - stringified object containing attachment data from the user
+        }
+    */
 
-expressApp.use(bodyParser.urlencoded({
-    extended: true
-}));
-expressApp.use(bodyParser.json());
-expressApp.post('/echo', function (req, res) {
-    var data = req.body.result.resolvedQuery;
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    return res.json({
-        speech: data,
-        displayText: data,
-        source: 'webhook-echo-sample',
-        data: data
-    });
+    // this is the object we will return to Motion AI in the callback
+    var responseJSON = {
+        "response": null, // what the bot will respond with
+        "continue": true, // "true" will result in Motion AI continuing the flow based on connections, whie "false" will make Motion AI hit this module again when the user replies
+        "customPayload": "", // OPTIONAL: working data to examine in future calls to this function to keep track of state
+        "quickReplies": null, // OPTIONAL: a JSON string array containing suggested/quick replies to display to the user .. i.e., ["Hello","World"]
+        "cards": null, // OPTIONAL: a cards JSON object to display a carousel to the user (see docs)
+        "customVars": event.customVars, // OPTIONAL: an object or stringified object with key-value pairs to set custom variables eg: {"key":"value"} or '{"key":"value"}'
+        "nextModule": null // OPTIONAL: the ID of a module to follow this Node JS module
+    }
     
-    var request = app.textRequest('', {
-        sessionId: 'aa25ee14-0efb-4292-9bdf-dccf69063db1'
-    });
     
-    request.on('response', function(response) {
-        console.log(response);
-        res.send(response);
-    });
-
-    request.on('error', function(error) {
-        console.log(error);
-        res.send(error);
-    });
-
-    request.end();
-})
-
-var server = expressApp.listen(process.env.PORT || 5000, function () {
-  var port = server.address().port;
-  console.log("Express is working on port " + port);
-});
+    console.log("type of Custom variable is: " + typeof(event.customVars))
+   
+    //console.log("evnt: " + event.customVars);
+    var myJSON = JSON.parse(event.customVars);
+    console.log(myJSON);
+    //console.log("name:" + myJSON.client);
+    console.log("myJSONissuetype: " + myJSON.issue);
+    //console.log(stringify(myJSON.issue));
+   
+    //console.log("contact type: " typeof(event.result));
+    
+    // callback to return data to Motion AI (must exist, or bot will not work)
+    callback(null, responseJSON);//responseJSON
+    
+};
